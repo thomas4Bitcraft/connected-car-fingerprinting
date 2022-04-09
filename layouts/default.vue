c2b24740a7d2a09d77bf8bb93401ac7825cea6d9
Thomas Brandauer <thomas@bitcraft.at>
2022-04-09 13:08:43 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: true,
+      fixed: true,
+      drawer: true,
+      items: [
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-content-save-all',
+          title: 'Save Car',
+          to: '/set-car',
+        },
+        {
+          icon: 'mdi-car-connected',
+          title: 'Check Car',
+          to: '/check-car',
+        },
+      ],
+      right: true,
+      rightDrawer: false,
+      title: 'Car Fingerprint Prototype',
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the car fingerprint prototype
+        </v-card-title>
+        <v-card-text>
+          <p>
+            It has already been shown in the past that cyberattacks are more successful the more the attacker knows about the system to be attacked. If someone knows a vulnerability in a certain software and version combination, it is easy to exploit this vulnerability for attacks. To gain this knowledge, fingerprinting methods are often used to identify the hardware and software used in a system in order to launch targeted attacks.
+            Due to the increasing number of internet-enabled vehicles, the growing threat of cyberattacks on vehicles and the experiences in the cybersecurity field regarding fingerprinting in the past, this prototype investigates what data can be extracted by visiting a website via the vehicle's internal browser. It is further investigated whether it is possible to create a specific fingerprint based on this data that can distinguish between manufacturer, model and software version of the vehicle.
+          </p>
+          <p>
+            Under <nuxt-link to="/save-car">Save Car</nuxt-link> you can help to expand our database by adding the publicly available browser data to our database and thus enable a more detailed analysis of the above mentioned topic.
+          </p>
+          <p>
+            Do you just want to check if your vehicle is recognized? Go to <nuxt-link to="/check-car">Check Car</nuxt-link>
+          </p>
+          <p>
+            As soon as the research is finished the paper will be available here:
+            <a
+              href="https://vuetifyjs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Link </a
+            >
+          </p>
+          <p>
+            If you have any questions, feel free to leave me a message.<br>
+            <a
+              href="mailto:ic18m020@technikum-wien.at"
+              rel="noopener noreferrer"
+              title="Contact"
+            >
+              ic18m020@technikum-wien.at
+            </a
+            >
+          </p>
+          <p>
+            Find a bug? Report it on the github
+            <a
+              href="https://github.com/vuetifyjs/vuetify/issues"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="contribute"
+            >
+              issue board</a
+            >.
+          </p>
+          <p>
+            Thank you for participating in the research to increase the safety of connected vehicles.
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; Thomas Brandauer</small></em>
+          </div>
+          <hr class="my-3" />
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <v-img
+              lazy-src="https://www.technikum-wien.at/sites/default/files/logo-300x160.png"
+              max-width="150"
+              src="https://www.technikum-wien.at/sites/default/files/logo-300x160.png"
+            ></v-img>
+            University of Applied Sciences Vienna
+          </a>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
