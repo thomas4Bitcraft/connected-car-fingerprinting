9eb9bb2c1f5e1308b1fa38bf5996163b304e9e76
Thomas Brandauer <thomas@bitcraft.at>
2022-04-10 18:43:39 +0200

    feat: logo

--- components/card/Footer.vue
+++ components/card/Footer.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <hr class="my-3" />
-    <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer">
+    <a href="https://www.technikum-wien.at/" target="_blank" rel="noopener noreferrer">
       <v-img
         lazy-src="https://www.technikum-wien.at/sites/default/files/logo-300x160.png"
         max-width="150"
         src="https://www.technikum-wien.at/sites/default/files/logo-300x160.png"
       ></v-img>
       <span class="pt-2 d-inline-block">
-        University of Applied Sciences Vienna
+        UAS Technikum Wien
       </span>
     </a>
   </div>
